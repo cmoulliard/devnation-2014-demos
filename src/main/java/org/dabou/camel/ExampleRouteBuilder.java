c53d6973f11b787ff354613f1203343327df7f98
Charles Moulliard <ch007m@gmail.com>
2014-03-20 19:41:03 +0100

    Presentation - Camel

---
+++ src/main/java/org/dabou/camel/ExampleRouteBuilder.java
@@ -0,0 +1,16 @@
+package org.dabou.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class ExampleRouteBuilder extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+        from("amq:queue:quotes")
+           .filter().xpath("/quote/product/ = 'widget")
+                .bean("QuotesService", "widget")
+           .filter().xpath("/quote/product/ = 'gadget")
+                .bean("QuotesService","gadget");
+
+    }
+}
---
+++ src/main/java/org/dabou/camel/QuotesService.java
@@ -0,0 +1,12 @@
+package org.dabou.camel;
+
+public class QuotesService {
+
+    public String widget() {
+        return "Hello Widget";
+    }
+
+    public String gadget() {
+        return "Hello Gadget";
+    }
+}
